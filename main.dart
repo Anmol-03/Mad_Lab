4f1d8ed8da6877d2016f6adf528d77ad42ad5528
Anmol-03 <113785823+Anmol-03@users.noreply.github.com>
2024-04-28 19:12:45 +0530

    Routing1->  main.dart

---
+++ main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'product.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Navigation Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductPage(), // Starting with the ProductPage
+    );
+  }
+}
